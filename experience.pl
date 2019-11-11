17be1421c35a5dddb1ae1173ad5a1befa90f5f8f
Siri Mykland <sirmy@samfundet.no>
2019-11-11 14:49:17 +0100

    experience works

--- experience.pl
+++ experience.pl
@@ -5,23 +5,24 @@ use File::Slurp;
 # open the html template
 my $template = HTML::Template->new(filename => 'test.tmpl');
 my $title = 'My experience';
-my $content = read_file('experience.html');
+my $content = HTML::Template->new(filename => 'experience.html');
 my @experience_list = [
     {name => 'Sam', 
      description => 'programmer',
-     icon => 'far fa-comments',
+     icon => "far fa-comments",
      color => 'color-2'}, 
     {name => 'Sam', 
      description => 'programmer',
-     icon => 'far fa-comments',
+     icon => "far fa-comments",
      color => 'color-3'}
     ];
 
 
 
 # fill in some parameters
+$content->param(EXPERIENCE => @experience_list);
 $template->param(TITLE => $title);
-$template->param(CONTENT => $content);
-$template->param( EXPERIENCE => @experience_list); 
+$template->param(CONTENT => $content->output);
+#$template->param( EXPERIENCE => @experience_list); 
 # send the obligatory Content-Type and print the template output
 print "Content-Type: text/html\n\n", $template->output;
